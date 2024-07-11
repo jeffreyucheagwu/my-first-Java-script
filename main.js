6c6e658a2228a0de1c856288a1ace139a020e267
admin <admin@admins-MacBook-Air.local>
2024-07-11 13:04:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// console.count("Welcome to JEFFREYCOM Partners");
+
+// const Gender; Any
+// Gender = "female";
+// console.log(Gender);
+
+// const FirstName = "Henry ";
+// const LastName = "James";
+// const age = 30;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(
+//   "My name is " + FirstName + LastName + "." + "I am " + age + " years old."
+// );
+// console.log(`My name is ${FirstName}${LastName} I am ${age} years old.`);
+// console.log(typeof isAdmin);
+
+// const FirstName = "Henry ";
+
+// const LastName = "James";
+// const age = 30;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(FirstName.toLowerCase());
+
+// const title = "welcome to my home";
+// console.log(title.length);
+// console.log(title.toUpperCase());
+// console.log(title.toLowerCase());
+// console.log(title.split(""));
+
+// const title = "welcome, ICT, Laptop, Router";
+// // console.log(title.length);
+// // console.log(title.toUpperCase());
+// // console.log(title.toLowerCase());
+// console.log(title.split(","));
+
+// const students = ["web", "home", "kitchen", 6, true];
+// students.pop();
+// students.unshift("computer");
+// students.push("table", "mouse", "maize");
+// students[6] = "gate";
+// console.log(students);
+const person = {
+  firstName: "Henry",
+  lastName: "Peter",
+  score: 40.5,
+  isAdmin: false,
+  hobbies: ["cooking", "Bet9ja", "boxing"],
+  address: {
+    city: "Owerri",
+    lga: "Owerri-West",
+    postalcode: 46013,
+  },
+};
+const { firstName, lastName } = person;
+console.log(firstName, lastName);
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.score);
+// console.log(person.isAdmin);
+// console.log(person.address.city);
+// console.log(person.hobbies[1]);
+// console.log(JSON.stringify(person));
